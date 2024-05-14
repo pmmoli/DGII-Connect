a031a6dec1a05c2eefcccfe9275c0374f405ef75
Michael Molina <michael.molina@outlook.com>
2024-05-14 17:05:14 -0400

    Initial Commit

---
+++ DTO/FormasPago.cs
@@ -0,0 +1,5 @@
+public class FormasPago
+{
+    public decimal MontoPago { get; set; }
+    public string? FormaPago { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DGII_Connect;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
